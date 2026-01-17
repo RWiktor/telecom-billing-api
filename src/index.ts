198b5bbfcee4f30443af347030995698587f4136
Wiktor Rybakowski <wiktor.rybakowski@gmail.com>
2026-01-17 01:49:28 +0100

    chore: set up express server with CORS and 404 error handling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express'
 import type { Express } from 'express'
+import type { Request, Response } from 'express'
+import cors from 'cors'
 
 const PORT = 8000
 const app: Express = express()
+
+app.use(cors())
+
+app.use((req: Request, res: Response): void => {
+  res.status(404).json({ message: 'No route found' })
+})
 
 app.listen(PORT, (): void => {
   console.log('Listening on port: ', PORT)
