b7ab1fe803f4caafd06687fa5ac69ace1b3c1fae
Wiktor Rybakowski <wiktor.rybakowski@gmail.com>
2026-01-19 23:24:12 +0100

    feat: add error handling middleware to the main application

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import { Request, Response } from 'express'
 import { usersRouter } from './routes/users.routes'
+import { errorHandler } from './middleware/error.middleware'
 
 const PORT = process.env.PORT || 8000
 const app = express()
@@ -15,6 +16,8 @@ app.use('/api/users', usersRouter)
 app.use((req: Request, res: Response): void => {
   res.status(404).json({ message: 'No route found' })
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, (): void => {
   console.log('Listening on port: http://localhost:' + PORT)
