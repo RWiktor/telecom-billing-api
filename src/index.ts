dac456caf601702695414e1dfbca87a3c0cc5400
Wiktor Rybakowski <wiktor.rybakowski@gmail.com>
2026-01-20 01:35:47 +0100

    feat: update routing to use indexRouter for API endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import { Request, Response } from 'express'
-import { usersRouter } from './routes/users.routes'
 import { errorHandler } from './middleware/error.middleware'
+import { indexRouter } from './routes'
 
 const PORT = process.env.PORT || 8000
 const app = express()
@@ -11,7 +11,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use('/api/users', usersRouter)
+app.use('/api', indexRouter)
 
 app.use((req: Request, res: Response): void => {
   res.status(404).json({ message: 'No route found' })
